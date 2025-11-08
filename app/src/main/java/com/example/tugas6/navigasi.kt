dae107bdbd813c8d31d35426eda572d435da3c17
sabilafirdias <sabilafirdias@gmail.com>
2025-11-08 12:46:07 +0700

    membuat enum class

---
+++ app/src/main/java/com/example/tugas6/navigasi.kt
@@ -0,0 +1,20 @@
+package com.example.tugas6
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.tugas6.view.FormDaftar
+import com.example.tugas6.view.HalamanDepan
+import com.example.tugas6.view.ListPeserta
+
+enum class Navigasi {
+    cover,
+    daftar,
+    formu
+}
+
